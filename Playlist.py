799a81cf60150ef3cde066b82dfc868e969b17f9
Xila Luna <xilaluna2@gmail.com>
2021-02-02 22:48:06 -0800

    final

--- Playlist.py
+++ Playlist.py
@@ -14,8 +14,17 @@ class Playlist:
     # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
 
     def find_song(self, title):
+        counter = 0
+        current_song = self.__first_song
+        while current_song.get_title() != title:
+            counter += 1
+            current_song = current_song.get_next_song()
+            if current_song == None:
+                return -1
+        if current_song.get_title() == title:
+            return counter
 
-        # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed.
+    # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed.
 
     def remove_song(self, title):
         current_song = self.__first_song
